555a0454ac17555b722f2b6d15de182ef12a5ec1
Albert Araque <aaraquedesarrollo@gmail.com>
2021-06-10 17:10:12 +0200

    completado sumar, restar y componente app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { useState } from "react";
+
 function App() {
+  const [contador, setContador] = useState(0);
+
+  const sumar = () => {
+    if (contador > 9) {
+      return;
+    }
+    setContador(contador + 1);
+  };
+  const restar = () => {
+    if (contador < 1) {
+      return;
+    }
+    setContador(contador - 1);
+  };
+
   return (
+    <div className="contenedor">
+      <div>
+        <button onClick={restar}>-</button>
+        <input type="number" value={contador} readOnly="true"></input>
+        <button onClick={sumar}>+</button>
+      </div>
+    </div>
   );
 }
 
